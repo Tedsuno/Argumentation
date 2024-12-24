cdc3e5edeb0c3f433420a756b96615ebc50b84b2
ayoub933 <129548409+ayoub933@users.noreply.github.com>
2024-12-24 20:45:52 +0100

    Update argumentation.py

--- argumentation.py
+++ argumentation.py
@@ -1,12 +1,12 @@
 import argparse
 from typing import List, Set, Tuple
-from itertools import chain, combinations, product
+from itertools import combinations, product
 
 class Argumentation:
     #Ici on initialise l'argumentation avec les arguments et les attaques
-    def __init__(self, arguments, attacks):
+    def __init__(self, arguments, attaque):
         self.arguments = arguments
-        self.attacks = attacks
+        self.attaque = attaque
 
     #Cette fonction permet de parser le fichier avec les arguments et les attaquants et cibles et les mettre dans
     #dans un résolveur d'argumentation
@@ -35,17 +35,19 @@ class Argumentation:
     #Cette fonction s'occupe de vérifier si un sous ensemble est sans conflit
     def est_sans_conflit(self, sousens):
     #Vérifier s'il existe une attaque dans les paires générées
-        return not any(map(lambda s: s in self.attacks, product(sousens, sousens)))
-    
+        return not any(map(lambda s: s in self.attaque, product(sousens, sousens)))
+        # {"a", "b", "c", "d", "e"},{("a", "b"),("b", "c"),("c", "d"),("d", "e")} 
+        # sousens = {"a", "c"}
+        # product(sousens, sousens) = [('a', 'a'), ('a', 'c'), ('c', 'a'), ('c', 'c')]
     
     #Cette fonction permet de savoir si un sous ensemble défend un arugment
     def defends(self, arg, sousens): 
-        for attaquant, cible in self.attacks:
+        for attaquant, cible in self.attaque:
             #On va chercher donc dans les attaques si l'argument est la cible d'une attaque
             if cible == arg:
                 #Si oui, on cherche s'il existe un défenseur à la cible
                 #Si elle n'existe pas on renvoie False
-                if not any((defenseur, attaquant) in self.attacks for defenseur in sousens):
+                if not any((defenseur, attaquant) in self.attaque for defenseur in sousens):
                     return False
         #Sinon True
         return True
@@ -53,11 +55,11 @@ class Argumentation:
     #Cette fonction verifie si un sous ensemble est admissible
     def est_admissible(self, sousens):
        #Pour être admissible il faut qu'il n'y ait pas de conflits et qu'il
-       # ne doit pas exister un argument dans sousens qui n'est pas défendu par sousens
-       return self.est_sans_conflit(sousens) and not any(not self.defends(arg, sousens) for arg in sousens)
+       # ne doit pas exister un argument dans sousens qui n'est pas défendu
+       return self.est_sans_conflit(sousens) and not any(not self.defends(arg, self.arguments) for arg in sousens)
     
     #Cette fonction renvoie les extensions complètes d'une argumentation
-    def complete_extensions(self):
+    def extensions_complete(self):
         extensions = []
         #Pour chaque sous ensemble de l'argumentation, on va vérifier comme dans le cours:
         for sousens in self.multi_ensemble():
@@ -71,14 +73,14 @@ class Argumentation:
         return extensions
 
     #Cette fonction renvoie les extensions stables d'une argumentation
-    def stable_extensions(self):
+    def extensions_stable(self):
         extensions = []
         #Pour chaque sous ensemble de l'argumentation, on va vérifier comme dans le cours:
         for sousens in self.multi_ensemble():
             #Premièrement, on vérifie s'il est sans conflit
             if self.est_sans_conflit(sousens):
                 # Deuxièmement, on cherche les arguments attaqués par le sous-ensemble
-                attaques = {cible for attaquant, cible in self.attacks if attaquant in sousens}
+                attaques = {cible for attaquant, cible in self.attaque if attaquant in sousens}
                 # Et enfin on vérifie si tout argument qui n'est pas dans sousens est attaqué
                 if all(arg in attaques  for arg in self.arguments - sousens):
                     extensions.append(sousens)
